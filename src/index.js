02adb16e9bf5b1be6ecc020bf9ab31f208967d60
ibouvousaime <ibouvousaime@gmail.com>
2025-04-27 18:18:52 +0200

    scheduler

--- src/index.js
+++ src/index.js
@@ -5,51 +5,61 @@ const { Bot } = require("grammy");
 const { sendRandomQuizz } = require("./trivia");
 const MongoDB = require("./mongo");
 const NodeCache = require("node-cache");
-
+const schedule = require("node-schedule");
 const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
 
-const timeToAnswer = 30;
+const timeToAnswer = 20;
 const myCache = new NodeCache();
-bot.command("trivia", async (ctx) => {
+
+bot.command("start", async (ctx) => {
 	const chatId = ctx.chat.id;
-	const lastUsedKey = `lastUsed-${chatId}`;
-	const lastUsed = myCache.get(lastUsedKey);
-	const cooldown = 10000;
-	if (lastUsed && Date.now() - lastUsed < cooldown) {
-		const remainingTime = Math.ceil((cooldown - (Date.now() - lastUsed)) / 1000);
-		ctx.reply(`Please wait ${remainingTime} seconds before the trivia is ready again.`);
+	const db = await MongoDB.getInstance().connect();
+	if (ctx.chat.type === "private") {
+		ctx.reply("This bot is not available in private chats. Please use it in a group chat.");
 		return;
-	} else {
-		let question;
-		let attempts = 0;
-		const maxAttempts = 3;
-
-		while (attempts < maxAttempts) {
-			try {
-				question = await sendRandomQuizz(ctx.chat.id);
-				if (question) break;
-			} catch (error) {
-				console.error(`Attempt ${attempts + 1} failed:`, error);
-			}
-			attempts++;
+	} else if (ctx.chat.type === "group" || ctx.chat.type === "supergroup") {
+		const chat = await db.collection("chats").findOne({ chatId });
+		if (!chat) {
+			await db.collection("chats").insertOne({ chatId, chatName: ctx.chat.title });
 		}
-
-		if (!question) {
-			ctx.reply("Failed to fetch a trivia question. Please try again later.");
-			return;
-		}
-		const pollMsg = await ctx.replyWithPoll(question.questionStr, question.options, {
-			type: "quiz",
-			correct_option_id: question.options.findIndex((option) => option === question.answer),
-			open_period: timeToAnswer,
-			is_anonymous: false,
-		});
-		const db = await MongoDB.getInstance().connect();
-		await db.collection("polls").insertOne({ ...pollMsg.poll, chatId: ctx.chat.id });
-		myCache.set(lastUsedKey, Date.now(), cooldown / 1000);
-		return;
 	}
 });
+
+const job = schedule.scheduleJob("0 */6 * * *", async () => {
+	const db = await MongoDB.getInstance().connect();
+	const chats = await db.collection("chats").find({}).toArray();
+	for (const chat of chats) {
+		sendQuizz(chat.chatId);
+		setTimeout(() => {
+			showLeaderboard(chat.chatId);
+		}, timeToAnswer * 1000);
+	}
+});
+async function sendQuizz(chatId) {
+	let question;
+	let attempts = 0;
+	const maxAttempts = 3;
+
+	while (attempts < maxAttempts) {
+		try {
+			question = await sendRandomQuizz(chatId);
+			if (question) break;
+		} catch (error) {
+			console.error(`Attempt ${attempts + 1} failed:`, error);
+		}
+		attempts++;
+	}
+
+	const pollMsg = await bot.api.sendPoll(chatId, question.questionStr, question.options, {
+		type: "quiz",
+		correct_option_id: question.options.findIndex((option) => option === question.answer),
+		open_period: timeToAnswer,
+		is_anonymous: false,
+	});
+	const db = await MongoDB.getInstance().connect();
+	await db.collection("polls").insertOne({ ...pollMsg.poll, chatId });
+	return;
+}
 
 bot.on("poll_answer", async (ctx) => {
 	const db = await MongoDB.getInstance().connect();
@@ -71,34 +81,26 @@ bot.on("poll_answer", async (ctx) => {
 		});
 });
 
-bot.command("leaderboard", async (ctx) => {
+async function showLeaderboard(chatId) {
 	const db = await MongoDB.getInstance().connect();
 	const leaderboard = await db
 		.collection("pollScores")
 		.aggregate([
-			{ $match: { chatId: ctx.chat.id } },
+			{ $match: { chatId } },
 			{ $addFields: { ratio: { $divide: ["$correctAnswers", { $add: ["$correctAnswers", "$wrongAnswers"] }] } } },
-			{ $sort: { ratio: -1 } },
+			{ $sort: { correctAnswers: -1 } },
 			{ $limit: 10 },
 		])
 		.toArray();
-
-	if (leaderboard.length === 0) {
-		ctx.reply("No scores available yet.");
-		return;
-	}
 	let message = "Leaderboard:\n";
 	for (let i = 0; i < leaderboard.length; i++) {
 		message += `<b>${i + 1}.</b> ${leaderboard[i].first_name} ${leaderboard[i].last_name ? leaderboard[i].last_name : ""} - <b>Score:</b> ${
-			leaderboard[i].ratio.toFixed(2) || 0
+			leaderboard[i].correctAnswers || 0
 		}\n`;
 	}
-	ctx.reply(message, { parse_mode: "HTML" });
-});
+	bot.api.sendMessage(chatId, message, { parse_mode: "HTML" });
+}
 
-bot.api.setMyCommands([
-	{ command: "trivia", description: "Get a trivia question" },
-	{ command: "leaderboard", description: "Get the leaderboard" },
-]);
+bot.api.setMyCommands([]);
 
 bot.start();
